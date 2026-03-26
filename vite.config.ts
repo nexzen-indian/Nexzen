b15077f543746c47f905c15a70a84a6067e2c3c4
Raj Kachhadiya <rajkachhadiya05@gmail.com>
2026-03-26 12:06:00 +0530

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/nexzen/',   // ← added this
+  base: '/Nexzen/',   // ← added this
   server: {
     host: "::",
     port: 8080,
